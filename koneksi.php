a25febc551a80d86f4a4289763b902c877da7352
Bagus Ali Akbar <bagusaliakbar77@gmail.com>
2025-06-23 10:32:21 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+	$host = "localhost";
+	$user = "root";
+	$password = "";
+	$nama_database = "db_bagus_npm";
+	$db = mysqli_connect($host, $user,$password, $nama_database);
+	// Mengecek apakah koneksi berhasil
+    //if (!$db) {
+        //die("Koneksi ke database gagal: " . mysqli_connect_error());
+    //} else {
+        //echo "Koneksi ke database berhasil.";
+    //}
+?>
+
